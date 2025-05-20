c28d69c91c99586d8909bd1dc31777c939beed7f
WillFerra <91874103+WillFerra@users.noreply.github.com>
2025-05-20 21:26:13 +0200

    1st Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.storjaar',
+  appName: 'storjaar',
+  webDir: 'www'
+};
+
+export default config;
